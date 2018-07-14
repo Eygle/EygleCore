7cb5146711a04f05b4b337b83c6af9f930ad7eac
eygle <cavalec.johan@gmail.com>
2018-07-14 16:04:42 +0200

    Restore (super failed)

--- commons/models/AModel.ts
+++ commons/models/AModel.ts
@@ -5,26 +5,4 @@ export class AModel {
     updateDate?: Date;
 
     deleted?: boolean;
-
-    /**
-     * Initialize object instance from data
-     * @param data
-     */
-    public initFromData(data: any) {
-        if (data) {
-            Object.assign(this, data);
-            this.formatDate('creationDate');
-            this.formatDate('updateDate');
-        }
-    }
-
-    /**
-     * Format date for given key
-     * @param {string} key
-     */
-    public formatDate(key: string) {
-        if (this[key]) {
-            this[key] = new Date(this[key]);
-        }
-    }
 }
