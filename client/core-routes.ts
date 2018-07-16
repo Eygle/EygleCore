f04767da0bf2768a70480806ce3e391036270683
Eygle <cavalec.johan@gmail.com>
2018-07-16 14:26:39 +0200

    Try to add some client features in core

---
+++ client/core-routes.ts
@@ -0,0 +1,40 @@
+import {LoginComponent} from './content/auth/login/login.component';
+import {RegisterComponent} from './content/auth/register/register.component';
+import {EPermission} from '../commons/core.enums';
+
+export const coreRoutes: [IRouteItem] = [
+
+  // Profile
+  // {
+  //   path: 'account',
+  //   component: HomeComponent,
+  //   translate: 'ACCOUNT.TITLE',
+  //   icon: 'account_circle',
+  //   access: EPermission.SeeAccount,
+  //   category: 'PROFILE'
+  // },
+  // {
+  //   path: 'settings',
+  //   component: HomeComponent,
+  //   translate: 'SETTINGS.TITLE',
+  //   icon: 'settings',
+  //   access: EPermission.SeeSettings,
+  //   category: 'PROFILE'
+  // },
+
+  // Auth
+  {path: 'auth/login', component: LoginComponent},
+  {path: 'auth/register', component: RegisterComponent},
+];
+
+export interface IRouteItem {
+  path: string;
+  component: any;
+
+  translate?: string;
+  icon?: string;
+  category?: string;
+  access?: EPermission;
+  exactMatch?: boolean;
+  url?: string;
+}
---
+++ client/core-routing.module.ts
@@ -0,0 +1,101 @@
+import {NavigationEnd, Router, RouterEvent} from '@angular/router';
+import {AuthService} from './services/auth.service';
+
+export class CoreRoutingModule {
+  /**
+   * Checks to perform
+   */
+  private _checks: Array<{ func: (url: string) => boolean, route: string }>;
+
+  /**
+   * Routes not to include in checks
+   */
+  private _ignoreRoutes: Array<String>;
+
+  /**
+   * List of errors routes (404, 500, ...)
+   */
+  private _errorsRoutes: Array<string>;
+
+  constructor(private router: Router, private auth: AuthService) {
+      this._checks = [
+          {func: this._shouldLogin, route: 'auth/login'},
+          {func: this._shouldNotSeeAuth, route: ''},
+          {func: this._shouldSeeResetPassword, route: 'auth/reset-password'}
+      ];
+
+      // Check if user need redirection on every navigation start
+      this.router.events.subscribe(
+          (event: RouterEvent) => {
+              if (event instanceof NavigationEnd) {
+                  this._performChecksNRedirects(event.url);
+              }
+          }
+      );
+  }
+
+  /**
+   * Perform all checks and redirects
+   * @private
+   * @param url
+   * @return {boolean}
+   */
+  private _performChecksNRedirects(url: string): void {
+    this._errorsRoutes = ['/errors/error-ie', '/errors/error-400', '/errors/error-500'];
+    this._ignoreRoutes = ['/auth/forgot-password', '/auth/login', '/auth/register'];
+
+    for (const check of this._checks) {
+      if (check.func.apply(this, [url])) {
+        if (check.hasOwnProperty('route')) { // Redirect to url
+          console.info('Redirect to route', check.route);
+          this.router.navigateByUrl(check.route);
+        }
+        return;
+      }
+    }
+  }
+
+  /**
+   * Should UserSchema be redirected to login view
+   * @param url
+   * @return {boolean}
+   * @private
+   */
+  private _shouldLogin(url) {
+    return !this.auth.isLogged() && !this._urlIn(this._ignoreRoutes, url);
+  }
+
+  /**
+   * Should be redirected to home because user is already logged in
+   * @param url
+   * @return {boolean}
+   * @private
+   */
+  private _shouldNotSeeAuth(url) {
+    return this.auth.isLogged() && this._urlIn(this._ignoreRoutes, url) &&
+      !this._urlIn(this._errorsRoutes, url) && url !== '/auth/forgot-password';
+  }
+
+  /**
+   * Should UserSchema be redirected to reset password view
+   * @param url
+   * @return {boolean}
+   * @private
+   */
+  private _shouldSeeResetPassword(url) {
+    this._ignoreRoutes.push('/auth/reset-password');
+    return !this._urlIn(this._ignoreRoutes, url) && this.auth.user.changePassword;
+  }
+
+  /**
+   * Is url in exclude list
+   * @param exclude
+   * @param {string} url
+   * @return {number}
+   * @private
+   */
+  private _urlIn(exclude: any, url: string) {
+    exclude = exclude instanceof Array ? exclude : [exclude];
+    return !!~exclude.indexOf(url);
+  }
+}
